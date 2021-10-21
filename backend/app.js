afdcb435ba21dd876bf06ef3955961500ad2bace
Lauri Niva <niva.lauri@gmail.com>
2021-10-21 10:17:47 +0300

    Add request logger to backend

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,21 @@ const app = express();
 import cloudinary from './utils/cloudinary.js';
 
 app.use(express.json({ limit: '5mb' }));
-app.use(express.urlencoded({ limit: '5mb', extended: true }))
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+};
+
+app.use(requestLogger);
 
 app.post('/api/upload', async (req, res) => {
   try {
-    const fileStr = req.body.data;
+    const fileStr = req.body.imagedata;
     const uploadResponse = await cloudinary.v2.uploader
       .upload(fileStr, { upload_preset: 'hunter_setup', });
     res.json(uploadResponse.public_id)
