abbcf325643e1f305ca5dbcf7a167d012ae27c74
Lauri Niva <niva.lauri@gmail.com>
2021-11-10 14:35:37 +0200

    Add mongoose connect

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,16 @@
 import express from 'express';
 const app = express();
+import mongoose from 'mongoose';
 import logsRouter from './controllers/logs.controllers.js';
+
+mongoose.connect(process.env.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    console.log('connected to mongodb');
+  })
+  .catch((error) => {
+    console.log('error conneting to db: ', error.message);
+  })
 
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
