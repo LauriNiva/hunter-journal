e5aaa61dedacdcf273cc18a5d434d334ffdf769e
Lauri Niva <niva.lauri@gmail.com>
2021-11-10 14:04:30 +0200

    Use own controller for logs

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-import cloudinary from './utils/cloudinary.js';
+import logsRouter from './controllers/logs.controllers.js';
 
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
@@ -15,17 +15,7 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger);
 
-app.post('/api/upload', async (req, res) => {
-  try {
-    const fileStr = req.body.imagedata;
-    const uploadResponse = await cloudinary.v2.uploader
-      .upload(fileStr, { upload_preset: 'hunter_setup', });
-    res.json(uploadResponse.public_id)
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Something went wrong with the upload'})
-  }
-})
+app.use('/api/upload', logsRouter);
 
 const PORT = process.env.PORT || 3001;
 
