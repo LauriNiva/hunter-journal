55313054677880ea2bbf43e41eff35b187a05f74
Lauri Niva <niva.lauri@gmail.com>
2022-01-26 12:19:31 +0200

    SingleLog component

---
+++ frontend/src/components/SingleLog.js
@@ -0,0 +1,10 @@
+// Yksittäisen login komponentti etusivulle
+// Logi saadaan Logs komponentilta
+
+import React from 'react';
+
+function SingleLog({ log }) {
+  return <div></div>;
+}
+
+export default SingleLog;
