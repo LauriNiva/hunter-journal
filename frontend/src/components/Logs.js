e878c798ebee55ff1160c489fa6b85355977655e
Lauri Niva <niva.lauri@gmail.com>
2022-01-26 12:19:21 +0200

    Logs component

---
+++ frontend/src/components/Logs.js
@@ -0,0 +1,11 @@
+//Komponentti käyttäjän kaikkien logien listaamiseen etusivulla
+//Logit saadaan App:lta listana
+//Ykisttäiset logit lähetetään SingleLog komponentille
+
+import React from 'react';
+
+function Logs({ logs }) {
+  return <div></div>;
+}
+
+export default Logs;
