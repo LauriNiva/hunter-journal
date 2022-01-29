a6e90fbff6d13c8d2ce0598d329c35c15f269f6e
Lauri Niva <niva.lauri@gmail.com>
2022-01-29 15:41:52 +0200

    Show image from log

--- frontend/src/components/SingleLog.js
+++ frontend/src/components/SingleLog.js
@@ -1,10 +1,19 @@
 // Yksittäisen login komponentti etusivulle
 // Logi saadaan Logs komponentilta
 
+import { Paper, Typography } from '@mui/material';
 import React from 'react';
 
 function SingleLog({ log }) {
-  return <div></div>;
+
+  const imageUrl = `https://res.cloudinary.com/devniva/image/upload/v1636547210/${log.images}`;
+
+  return (
+    <Paper elevation={6}>
+      <Typography>{log.animal}</Typography>
+      <img src={imageUrl} width="300" />
+    </Paper>
+  );
 }
 
 export default SingleLog;
