22303bf71fc1b109c72a6736b86ad2185e81a02c
Lauri Niva <niva.lauri@gmail.com>
2022-01-30 09:18:33 +0200

    Styling for the log

--- frontend/src/components/SingleLog.js
+++ frontend/src/components/SingleLog.js
@@ -6,12 +6,19 @@ import React from 'react';
 
 function SingleLog({ log }) {
 
-  const imageUrl = `https://res.cloudinary.com/devniva/image/upload/v1636547210/${log.images}`;
+
+  const imageUrl = `https://res.cloudinary.com/devniva/image/upload/v1636547210/${log.images[0]}`;
+
+  const badgeColors = {'None': '#000000', 'Bronze': '#824A02', 'Silver': '#000000', 'Gold': '#000000', 'Diamond': '#000000', 'Great One': '#B9F2FF'}
+
+  const badgeColor = badgeColors[log.badge];
+
+
 
   return (
     <Paper elevation={6}>
-      <Typography>{log.animal}</Typography>
-      <img src={imageUrl} width="300" />
+      <Typography variant="h4">{log.animal}</Typography>
+      <Typography variant="h5">{log.rating}</Typography>
     </Paper>
   );
 }
