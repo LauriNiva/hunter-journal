12248400bab111101e2e0ba50f0e756526fd0f59
Lauri Niva <niva.lauri@gmail.com>
2022-01-31 18:41:04 +0200

    Badge colors for rating

--- frontend/src/components/SingleLog.js
+++ frontend/src/components/SingleLog.js
@@ -9,16 +9,23 @@ function SingleLog({ log }) {
 
   const imageUrl = `https://res.cloudinary.com/devniva/image/upload/v1636547210/${log.images[0]}`;
 
-  const badgeColors = {'None': '#000000', 'Bronze': '#824A02', 'Silver': '#A7A7AD', 'Gold': '#FEE101', 'Diamond': '#B9F2FF', 'Great One': '#B9F2FF'}
+  const badgeColors = {
+    'None': '#000000',
+    'Bronze': '#824A02',
+    'Silver': '#A7A7AD',
+    'Gold': '#FEE101',
+    'Diamond': '#B9F2FF',
+    'Great One': '#B9F2FF'
+  };
 
-  const badgeColor = badgeColors[log.badge];
+  const logBadgeColor = badgeColors[log.badge];
 
 
 
   return (
-    <Paper elevation={6}>
+    <Paper sx={{ margin: 2, padding: 2 }} elevation={6}>
       <Typography variant="h4">{log.animal}</Typography>
-      <Typography variant="h5">{log.rating}</Typography>
+      <Typography sx={{color: logBadgeColor}} variant="h5">{log.rating}</Typography>
     </Paper>
   );
 }
