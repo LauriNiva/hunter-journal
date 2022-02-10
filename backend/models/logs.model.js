1e752be80ef1d8a465cc7dfd4004c85bc9177fd8
Lauri Niva <niva.lauri@gmail.com>
2022-02-10 09:48:50 +0200

    Add user to logs model

--- backend/models/logs.model.js
+++ backend/models/logs.model.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
 
 const logSchema = new mongoose.Schema({
+  user: { 
+    type: String,
+    required: true
+  },
   animal: {
     type: String,
     required: true
