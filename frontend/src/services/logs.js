a512554876e0e85ad6ddbc4d3cbbfd4c5a0b727f
Lauri Niva <niva.lauri@gmail.com>
2022-02-10 09:49:41 +0200

    Add token to the fetch

--- frontend/src/services/logs.js
+++ frontend/src/services/logs.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 
+
 const baseURL = "http://localhost:3001/api/";
 
-const getAllLogs = async () => {
-  const allLogs = await axios.get(`${baseURL}`);
+const getAllLogs = async (token) => {
+  const allLogs = await axios
+    .get(`${baseURL}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+      },
+    );
   return allLogs.data;
 };
 
