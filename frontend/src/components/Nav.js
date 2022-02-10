3ec904bce712b3e03ddbaa80beebba8f6e0f46ac
Lauri Niva <niva.lauri@gmail.com>
2022-02-10 09:50:13 +0200

    Authbutton and username to the navbar

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import AuthButton from './AuthButton';
+import { useAuth0 } from '@auth0/auth0-react'
+
 
 function Nav() {
-  return <div>
-    Navbar
-  </div>;
+
+  const { user, isAuthenticated } = useAuth0();
+
+  return (
+  <div>
+    <div>Hunter Log</div>
+
+    <div>
+      {isAuthenticated && user.name}
+      <AuthButton />
+    </div>
+  </div>
+  )
 }
 
 export default Nav;
