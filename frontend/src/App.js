73bed6885dd4bbf7a5674523a07939408d0d4d51
Lauri Niva <niva.lauri@gmail.com>
2022-02-10 09:52:29 +0200

    Import Nav Authentication before getting logs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,28 @@ import './App.css';
 import logsService from './services/logs';
 import NewLogForm from './components/NewLogForm';
 import Logs from './components/Logs';
+import Nav from './components/Nav';
+import { useAuth0 } from '@auth0/auth0-react';
+
 
 const App = () => {
 
+  const { getAccessTokenSilently, isAuthenticated } = useAuth0();
+
   const [logs, setLogs] = useState([]);
 
-  useEffect( () => {
+  useEffect(() => {
+    isAuthenticated &&
     (async () => {
-      setLogs( await logsService.getAllLogs());
+      const token = await getAccessTokenSilently();
+      setLogs(await logsService.getAllLogs(token));
     })()
-  }, []);
+  }, [isAuthenticated]);
 
   return (
     <div>
-      <NewLogForm/>
+      <Nav />
+      <NewLogForm />
       <Logs logs={logs} />
 
     </div>
