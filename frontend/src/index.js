10feef6ddc3bf477abb767b79a067df7a7552e2a
Lauri Niva <niva.lauri@gmail.com>
2022-02-11 09:52:58 +0200

    Remove extra semicolon

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Auth0ProviderWithHistory from './auth/auth0-provider-with-history';;
+import Auth0ProviderWithHistory from './auth/auth0-provider-with-history';
+
+
 
 
 ReactDOM.render(
   <Router>
     <Auth0ProviderWithHistory>
-      <App />
+        <App />
     </Auth0ProviderWithHistory>
   </Router>,
   document.getElementById('root')
