6e08ef88fce6d0c8ab09c0ec27d09e403ffcff9a
Lauri Niva <niva.lauri@gmail.com>
2022-02-12 09:35:36 +0200

    Plans for controller

--- frontend/src/services/logs.js
+++ frontend/src/services/logs.js
@@ -3,6 +3,11 @@ import axios from "axios";
 
 const baseURL = "http://localhost:3001/api/";
 
+//Hae tykätyimmät logit etusivulle
+
+//Tykkää logista
+
+//Hae käyttäjän omat logit
 const getAllLogs = async (token) => {
   const allLogs = await axios
     .get(`${baseURL}`,
@@ -14,6 +19,13 @@ const getAllLogs = async (token) => {
     );
   return allLogs.data;
 };
+
+//Lisää logi
+
+//Poista logi
+
+//Päivitä logia
+
 
 const logsService = { getAllLogs };
 
