4d410349f3fe4190f9d7c55ff2f18ab4dd46ad29
Lauri Niva <niva.lauri@gmail.com>
2022-02-13 19:06:54 +0200

    Sorting for the log components

--- frontend/src/components/Logs.js
+++ frontend/src/components/Logs.js
@@ -2,22 +2,68 @@
 //Logit saadaan App:lta listana
 //Ykisttäiset logit lähetetään SingleLog komponentille
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SingleLog from './SingleLog';
+import NewLogForm from './NewLogForm.js';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Button, Typography } from '@mui/material';
 
-function Logs({ logs }) {
+
+
+function Logs({ logs, setLogs }) {
+
+  const { isAuthenticated } = useAuth0();
+
+  const [logsToDisplay, setLogsToDisplay] = useState([]);
+  const [selectedSortForLogs, setSelectedSortForLogs] = useState('Newest First');
+  
+  const sortsForLogs = ['Newest First', 'Oldest First', 'Highest Rating', 'Lowest Rating'];
+
+  const sortLogs = () => {
+    let sortedLogs = logs;
+
+    if(selectedSortForLogs === 'Newest First') {
+      sortedLogs = [...logs].sort((a,b) => Date.parse(a.createdAt) - Date.parse(b.createdAt));
+    }else if(selectedSortForLogs === 'Oldest First'){
+      sortedLogs = [...logs].sort((a,b) => Date.parse(b.createdAt) - Date.parse(a.createdAt));
+    }else if(selectedSortForLogs === 'Highest Rating'){
+      sortedLogs = [...logs].sort((a,b) => a.rating - b.rating);
+    }else if(selectedSortForLogs === 'Lowest Rating'){
+      sortedLogs = [...logs].sort((a,b) => b.rating - a.rating);
+    }
+    console.log('logs', logs)
+    console.log('sortedLogs', sortedLogs)
+
+
+    setLogsToDisplay(sortedLogs)
+  };
+
+  useEffect(() => {
+    console.log("useEffect for sort", selectedSortForLogs)
+    sortLogs();
+  },[logs, selectedSortForLogs]);
+
+
 
 
   return (
 
-    <div>[All logs bar - Filter - Arrange - New Log Button]
+    isAuthenticated
+      ? <div>
+        {sortsForLogs.map(sort =>
+          <Button key={sort} id={`button${sort.split(' ').join('')}`} onClick={() => setSelectedSortForLogs(sort)}>
+            {sort}
+          </Button>
+        )}
+        <NewLogForm setLogs={setLogs} />
 
-  {
-    logs.map(log => (
-      <SingleLog key={log._id} log={log} />
-  ))
-  }
-  </div>
+        {
+          logsToDisplay.map(log => (
+            <SingleLog key={log._id} log={log} />
+          ))
+        }
+      </div>
+      : <Typography>Please log in</Typography>
   );
 }
 
