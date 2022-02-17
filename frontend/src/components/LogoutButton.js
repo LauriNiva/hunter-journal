22a7e8f154223d6ad30d57f063f78dff5b94fde3
Lauri Niva <niva.lauri@gmail.com>
2022-02-17 12:31:08 +0200

    Use icon as logout button

--- frontend/src/components/LogoutButton.js
+++ frontend/src/components/LogoutButton.js
@@ -2,19 +2,20 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from '@mui/material';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
   return (
     <Button
-    color="error"
+    color="primary"
       onClick={() =>
         logout({
           returnTo: window.location.origin,
         })
       }
     >
-      Log Out
+      <LogoutIcon />
     </Button>
   );
 };
