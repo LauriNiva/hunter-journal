aceb9bdb39051213e4a1431dc01bade0874054fc
Lauri Niva <niva.lauri@gmail.com>
2022-02-22 19:50:33 +0200

    Controller for get user with id and creating new

---
+++ backend/controllers/users.controller.js
@@ -0,0 +1,46 @@
+import User from '../models/user.model.js';
+import express from 'express';
+import checkJwt from '../middleware/jwtCheck.js';
+
+
+const usersRouter = express.Router();
+
+usersRouter.get('/', checkJwt, async (req, res) => {
+  try {
+    console.log('req.user.sub', req.user.sub)
+    const user = await User.findById(req.user.sub)
+    res.json(user);
+  } catch (error) {
+    res.status(404).end();
+  }
+});
+
+usersRouter.post('/', checkJwt, async (req, res) => {
+
+  const body = req.body;
+  try {
+
+    const newUser = new User(
+      {
+        username: body.username,
+        email: body.email,
+        _id: req.user.sub
+      }
+    );
+
+
+    const createdUser = await newUser.save();
+
+
+    res.json(createdUser)
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Something went wrong with the user creation' })
+  }
+
+
+});
+
+
+export default usersRouter;
