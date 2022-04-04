245f95f9a3b038ec93dee602556f08f83d93c77b
Lauri Niva <niva.lauri@gmail.com>
2022-04-04 20:11:18 +0300

    Change mobile layout

--- frontend/src/components/Frontpage.js
+++ frontend/src/components/Frontpage.js
@@ -25,35 +25,40 @@ function Frontpage({ likedLogs, setLikedLogs }) {
 
 
   return (
-      <Box sx={{
-        display: "grid",
-        gridTemplateColumns: "1fr 1fr",
-        gridTemplateRows: "100px 1fr",
-        gridTemplateAreas: `"head head"
+    <Box sx={{
+      display: "grid",
+      gridTemplateColumns: "1fr 1fr",
+      gridTemplateRows: { sm: "100px 1fr"},
+      gridTemplateAreas: {
+        xs: `"head head"
+          "new new"
+          "liked liked"
+          `,
+        sm: `"head head"
         "new liked"
-        `,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}>
-        <Box sx={{ gridArea: 'head', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <Typography sx={{ m: 1, p: 1, fontSize: '20px' }}>
-            Site to log your harvests from the game
-            <Typography variant='body1' variantMapping={{ body1: 'span' }} sx={{ fontSize: '20px', display: 'inline', fontFamily: 'Jaapokki' }}> theHunter: Call of the Wild</Typography>.
-            <br /> In The Lodge you can follow fellow hunters and see their recent logs.
-          </Typography>
+        `},
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <Box sx={{ gridArea: 'head', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Typography sx={{ m: 1, p: 1, fontSize: '20px' }}>
+          Site to log your harvests from the game
+          <Typography variant='body1' variantMapping={{ body1: 'span' }} sx={{ fontSize: '20px', display: 'inline', fontFamily: 'Jaapokki' }}> theHunter: Call of the Wild</Typography>.
+          <br /> In The Lodge you can follow fellow hunters and see their recent logs.
+        </Typography>
 
-        </Box>
-
-        <Box className="hidden-scroll" sx={{ gridArea: 'new', maxHeight: '74vh', overflow: 'scroll',}}>
-          <Typography variant="h6" sx={{ ml: 3 }} >Recently added logs</Typography>
-          {mostRecentLogs.map(log => <SingleLog key={`recentfp${log._id}`} log={log} likedLogs={likedLogs} setLikedLogs={setLikedLogs} dataToShow='user' />)}
-        </Box>
-
-        <Box className="hidden-scroll" sx={{ gridArea: 'liked', maxHeight: '74vh', overflow: 'scroll', }}>
-          <Typography variant="h6" sx={{ ml: 3 }} >Most liked logs</Typography>
-          {mostLikedLogs.map(log => <SingleLog key={`likedfp${log._id}`} log={log} likedLogs={likedLogs} setLikedLogs={setLikedLogs} dataToShow='likes' />)}
-        </Box>
       </Box>
+
+      <Box className="hidden-scroll" sx={{ gridArea: 'new', maxHeight: '74vh', overflow: { sm: 'scroll' }, }}>
+        <Typography variant="h6" sx={{ ml: 3 }} >Recent logs</Typography>
+        {mostRecentLogs.map(log => <SingleLog key={`recentfp${log._id}`} log={log} likedLogs={likedLogs} setLikedLogs={setLikedLogs} dataToShow='user' />)}
+      </Box>
+
+      <Box className="hidden-scroll" sx={{ gridArea: 'liked', maxHeight: '74vh', overflow: { sm: 'scroll' }, }}>
+        <Typography variant="h6" sx={{ ml: 3 }} >Most liked logs</Typography>
+        {mostLikedLogs.map(log => <SingleLog key={`likedfp${log._id}`} log={log} likedLogs={likedLogs} setLikedLogs={setLikedLogs} dataToShow='likes' />)}
+      </Box>
+    </Box>
   )
 }
 
