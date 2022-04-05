6dc2628e768c41b1a3211c187da82ae5dce2b5e3
Lauri Niva <niva.lauri@gmail.com>
2022-04-05 07:51:57 +0300

    Lodge styling

--- frontend/src/components/Lodge.js
+++ frontend/src/components/Lodge.js
@@ -30,16 +30,17 @@ function Lodge({ followedUsers, likedLogs, setLikedLogs, followedUseravatars }) 
 
 
     return (
-      <Paper elevation={4} sx={{ gridArea: 'followed', }}>
+      <Paper elevation={5} sx={{ gridArea: 'followed', pt: 2 }}>
         <UserSearch />
         <Typography align="center" sx={{ marginTop: 4 }} >Hunters you follow</Typography>
-        <Container disableGutters sx={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}} >
-        {followedUsers.map(user =>
-          <Button sx={{textTransform: 'none', fontFamily: 'Jaapokki'}} onClick={() => navigate(`/hunters/${user}`)} key={`following-${user}`}>
-            <Avatar sx={{ width: 30, height: 30, mr: 1, ml: 1 }} src={`https://avatars.dicebear.com/api/identicon/${followedUseravatars[user]}.svg?scale=85`} alt={`${user}avatar`} />
-            {user}
-          </Button>
-        )}
+        <Container disableGutters sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center',
+         maxHeight: {xs: '50vh', sm: 'auto' }, p: 2 }} >
+          {followedUsers.map(user =>
+            <Button sx={{ textTransform: 'none', fontFamily: 'Jaapokki' }} onClick={() => navigate(`/hunters/${user}`)} key={`following-${user}`}>
+              <Avatar sx={{ width: 30, height: 30, mr: 1, ml: 1 }} src={`https://avatars.dicebear.com/api/identicon/${followedUseravatars[user]}.svg?scale=85`} alt={`${user}avatar`} />
+              {user}
+            </Button>
+          )}
         </ Container>
       </Paper>
     )
@@ -52,21 +53,29 @@ function Lodge({ followedUsers, likedLogs, setLikedLogs, followedUseravatars }) 
   return (
     <Container disableGutters sx={{
       display: 'grid',
-      gridTemplateColumns: "2fr 3fr",
-      gridTemplateRows: "100px 1fr",
-      gridTemplateAreas: `"head head"
-    "followed recent"
-    ` }}>
-      <Container sx={{ gridArea: 'head', }}>
+      gridTemplateColumns: {sm: "2fr 3fr"},
+      gridTemplateRows: {sm: "100px 1fr"},
+      gridTemplateAreas: {
+        xs:
+      `" head"
+      "followed"
+      "recent"` ,
+        sm:
+      `"head head"
+      "followed recent"
+    `
+      }
+    }}>
+      <Container sx={{ gridArea: 'head', mt:2, mb: 2 }}>
         <Typography align="center" variant="h4" sx={{ fontFamily: 'Jaapokki' }}>The Lodge</Typography>
         <Typography align="center" >Lodge is the place to find and follow your fellow hunters.
           Here you can easily see recent logs from the hunters you follow.</Typography>
       </Container>
       <ListOfFollowedUsers />
 
-      <Paper sx={{ gridArea: 'recent', width: '100%', }}>
-        <Typography variant="h5" align="center" sx={{ fontFamily: 'Jaapokki' }} >Recent Logs</Typography>
-        <Container className="hidden-scroll" disableGutters sx={{ overflow: 'scroll', height: '75vh', }} >
+      <Paper elevation={5} sx={{ gridArea: 'recent', mt: {xs: 2, sm: 0}, ml: {xs: 0, sm: 1}, pt:1,  }}>
+        <Typography variant="h5" align="center" sx={{ fontFamily: 'Jaapokki' }} >Recent Logs from followed</Typography>
+        <Container className="hidden-scroll" disableGutters sx={{ overflow: {sm: 'scroll'}, height: {sm: '75vh'}, }} >
           {
             recentFollowedLogs.map(log =>
               <SingleLog key={`recentlodge${log._id}`} log={log} dataToShow={'user'} likedLogs={likedLogs} setLikedLogs={setLikedLogs} />
